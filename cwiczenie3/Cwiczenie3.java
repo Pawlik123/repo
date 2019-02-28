3be73898caf1d3a2f31d18f8c75011bb0f880cd9
Pawlik123 <elzbieta1@opoczta.pl>
2019-02-28 19:00:33 +0100

    blabla

--- cwiczenie3/Cwiczenie3.java
+++ cwiczenie3/Cwiczenie3.java
@@ -8,7 +8,8 @@ public class Cwiczenie3 {
 		System.out.println("Podaj liczbê A");
 		Scanner in=new Scanner(System.in);
 		int liczba=in.nextInt();
-				
+		System.out.println ("Podaj liczbê B");
+		int liczba2=in.nextInt();
 	}
 
 }
