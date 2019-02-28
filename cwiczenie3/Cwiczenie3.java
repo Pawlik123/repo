27f3625e1b1c42fbe024060e76d69b22a107d194
Pawlik123 <elzbieta1@opoczta.pl>
2019-02-28 18:55:43 +0100

    blabla

---
+++ cwiczenie3/Cwiczenie3.java
@@ -0,0 +1,14 @@
+package cwiczenie3;
+
+import java.util.Scanner;
+
+public class Cwiczenie3 {
+	public static void main(String[] args)
+	{
+		System.out.println("Podaj liczbê A");
+		Scanner in=new Scanner(System.in);
+		int liczba=in.nextInt();
+				
+	}
+
+}
