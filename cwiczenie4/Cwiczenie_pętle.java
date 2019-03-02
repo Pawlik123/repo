fe0732acf9a322c6298bccc82ca83a2a9ebb1949
Pawlik123 <elzbieta1@opoczta.pl>
2019-03-02 16:25:56 +0100

    liczby_parzyste

---
+++ cwiczenie4/Cwiczenie_pętle.java
@@ -0,0 +1,12 @@
+package cwiczenie4;
+
+public class Cwiczenie_pêtle {
+	public static void main(String[] args)
+		{
+			System.out.println("Wypisz liczby parzyste do 10 w³¹cznie");
+			for (int b=0; b<=10; b=b+2)
+			{
+				System.out.println(b);
+			}
+		}
+	}
