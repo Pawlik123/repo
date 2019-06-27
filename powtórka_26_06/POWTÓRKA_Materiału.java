903237b851da1c476276e7d4c06248a593743ebc
Pawlik123 <elzbieta1@opoczta.pl>
2019-06-27 21:00:43 +0200

    Powtórka

---
+++ powtórka_26_06/POWTÓRKA_Materiału.java
@@ -0,0 +1,16 @@
+package powtórka_26_06;
+
+public class POWTÓRKA_Materia³u {
+	public static void main(String[] args){
+		int a=5;
+		int b=3;
+		System.out.println("a+b="+(a+b));
+	String helo="Witaj";
+	String word="Swicie";
+	String x=helo+word;
+	System.out.println(x);
+	String Czeœæ=x.substring(0,5)+" uczniu";
+	System.out.println(Czeœæ);
+}
+
+}
