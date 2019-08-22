f37d58790c072201c819d657a4832a99a527b58b
Pawlik123 <elzbieta1@opoczta.pl>
2019-08-22 10:44:21 +0200

    Tablice

---
+++ petle/Zadanie_tablice.java
@@ -0,0 +1,12 @@
+package petle;
+
+public class Zadanie_tablice {
+	public static void main(String[] args){
+		String[] names={"Marcin", "Monika", "Daniel"};
+		for(int i=0; i<names.length; i++)
+		{
+			System.out.println(names[i]);
+		}
+	}
+
+}
