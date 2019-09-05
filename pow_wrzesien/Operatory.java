f3eb14cafd1293161a7da842fa8f4ca51fefa316
Pawlik123 <elzbieta1@opoczta.pl>
2019-09-05 15:38:39 +0200

    operatory

---
+++ pow_wrzesien/Operatory.java
@@ -0,0 +1,20 @@
+package pow_wrzesien;
+
+public class Operatory {
+	public static void main(String[] args)
+	{
+		int a=17;
+		int b=5;
+		int c=a+b;
+		System.out.println(c);
+		c=a-b;
+		System.out.println(c);
+		c=a*b;
+		System.out.println(c);
+		c=a/b;
+		System.out.println(c);
+		c=a%b;
+		System.out.println(c);
+	}
+
+}
