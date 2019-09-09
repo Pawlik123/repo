e39e0a5247488166afc46587d6a8544859ba54e3
Pawlik123 <elzbieta1@opoczta.pl>
2019-09-09 12:11:23 +0200

    Metody

---
+++ pow_wrzesien/Zadanie_kolejne.java
@@ -0,0 +1,28 @@
+package pow_wrzesien;
+
+import java.util.Scanner;
+
+public class Zadanie_kolejne {
+public static void main(String[] args);
+{
+System.out.println("Napisz ile dni ma dany mc w roku");
+Scanner in= new Scanner(System.in);
+int nr=in.nextInt()
+switch(nr);
+case 1:
+	
+case 3:
+case 5:
+case 7:
+case 8:
+case 10:
+case 12:
+System.out.println("Ten mc ma 31 dni");
+break;
+case 2:
+System.out.println("Ten mc ma 28 dni");
+default:
+System.out.println("Ten mc ma 30 dni")
+}
+}
+}
