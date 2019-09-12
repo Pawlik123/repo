4fd00dfeb6a8303b77405a83d7710789d5506dec
Pawlik123 <elzbieta1@opoczta.pl>
2019-09-12 10:14:53 +0200

    Zadanie

---
+++ powtorka_petla/Zadanie.java
@@ -0,0 +1,12 @@
+package powtorka_petla;
+
+public class Zadanie {
+	public static void main(String[] args)
+	{
+		for (int i=0; i<11; i=i+2)
+		{
+			System.out.println("Liczby parzyste to " +i );
+		}
+	}
+
+}
