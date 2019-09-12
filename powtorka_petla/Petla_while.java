64893b9573e8c12b3b8b827c3d058b719b8e7f4f
Pawlik123 <elzbieta1@opoczta.pl>
2019-09-12 09:57:10 +0200

    Petle

---
+++ powtorka_petla/Petla_while.java
@@ -0,0 +1,14 @@
+package powtorka_petla;
+
+public class Petla_while {
+	public static void main(String[] args)
+	{
+		int licznik=0;
+		while(licznik<10){
+			System.out.println("To jest petla");
+			licznik++;
+		}
+		System.out.println("Koniec petli");
+	}
+
+}
---
+++ powtorka_petla/Pętle.java
@@ -0,0 +1,15 @@
+package powtorka_petla;
+
+public class Pêtle {
+	public static void main(String[] args)
+	{
+		int qq=6;
+		do
+		{
+			System.out.println("Wydrukuj");
+			qq++;
+		}
+		while(qq<10);
+	}
+
+}
