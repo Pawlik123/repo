1de7ca7d18534f9ae292088517c44b73a9ba3029
Pawlik123 <elzbieta1@opoczta.pl>
2019-10-25 11:36:32 +0200

    ćwiczenia petla

---
+++ powtorka_pazdziernik/Petle.java
@@ -0,0 +1,14 @@
+package powtorka_pazdziernik;
+
+public class Petle {
+	public static void main(String[] args)
+	{
+		int licznik=0;
+		while(licznik<10){
+			System.out.println("Pêtla");
+			licznik++;
+		}
+		System.out.println("Koniec ");
+	}
+
+}
