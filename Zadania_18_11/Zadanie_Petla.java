5027c695d717ba075b6f5d8686f278b000938dc6
Pawlik123 <elzbieta1@opoczta.pl>
2019-11-21 15:06:56 +0100

    zadanie

---
+++ Zadania_18_11/Zadanie_Petla.java
@@ -0,0 +1,19 @@
+package Zadania_18_11;
+
+public class Zadanie_Petla {
+	public static void main(String[] args)
+	{
+		int tab[]=new int[12];
+		for(int i=0; i<12; i++)
+		{
+			
+		tab[i]=i+1;
+		}
+			
+			for (int y: tab)
+		{
+			System.out.println(y);	
+		}
+	}
+
+}
---
+++ Zadania_18_11/Zadanie_Random.java
@@ -0,0 +1,15 @@
+package Zadania_18_11;
+
+import java.util.Random;
+
+public class Zadanie_Random {
+	public static void main(String[] args)
+	{
+		Random eee=new Random();
+		for(int i=0; i<20; i++);
+		{
+		System.out.println("Liczby to" + eee.nextInt());	
+		}
+	}
+
+}
